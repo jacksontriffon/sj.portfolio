500c5daae20dab3387dcb749422adb371f4da910
jacksontriffon <jackson.triffon@gmail.com>
2024-01-26 11:13:26 +1100

    feat: remove defaults

---
+++ src/lib/Nav.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import NavButton from "./NavButton.svelte";
+
+    
+</script>
+
+<nav class="header">
+    <NavButton buttonText="SJ's Portfolio" />
+    <div></div>
+    <NavButton buttonText="About" />
+    <div></div>
+    <NavButton buttonText="Contact" />
+</nav>
+
+<style>
+    nav {
+        width: 100%;
+        display: flex;
+        gap: 16px;
+        align-items: center;
+    }
+
+    div {
+        width: 8px;
+        height: 8px;
+        background: var(--neutral-500);
+        border-radius: 50%;
+    }
+</style>
---
+++ src/lib/NavButton.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    export let buttonText: string
+</script>
+
+<button>
+    {buttonText}
+</button>
+
+<style>
+    button {
+        background: none;
+        font-size: 1.5rem;
+        border: none;
+        padding-left: 8px;
+        padding-right: 8px;
+        padding-top: 4px;
+        padding-bottom: 4px;
+    }
+</style>
