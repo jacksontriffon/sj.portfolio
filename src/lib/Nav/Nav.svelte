48109cc20ef09adea34995476cfe0f083512b19a
jacksontriffon <jackson.triffon@gmail.com>
2024-02-10 16:40:06 +1100

    feat: slight nav changes

--- src/lib/Nav/Nav.svelte
+++ src/lib/Nav/Nav.svelte
@@ -21,10 +21,10 @@
 				href="/documents/SJ's Resume.pdf"
 				download="SJs-resume.pdf"
 				on:mouseenter={() => {
-					anchorTagColor = "var(--neutral-200)";
+					anchorTagColor = "white";
 				}}
 				on:mouseleave={() => {
-					anchorTagColor = "var(--neutral-500)";
+					anchorTagColor = "var(--neutral-200)";
 				}}
 			>
 				Download Resume
@@ -54,15 +54,33 @@
 		justify-content: center;
 		align-items: center;
 		gap: 8px;
-		color: var(--neutral-500);
+		/* background-color: rgba(0, 0, 0, 0.3); */
+		/* backdrop-filter: blur(8px); */
+		color: var(--neutral-200);
 		transition: color 0.2s;
+		-webkit-text-stroke: 0.01px black;
+		text-shadow:
+			1px 1px 0 #000,
+			-1px -1px 0 #000,
+			1px -1px 0 #000,
+			-1px 1px 0 #000,
+			1px 1px 0 #000;
 	}
 
 	a:hover {
-		color: var(--neutral-200);
+		color: white;
 	}
 
 	a:active {
-		color: white;
+		color: var(--neutral-200);
+	}
+
+	@media only screen and (max-width: 450px) {
+		nav {
+			top: 16px;
+		}
+		nav div {
+			padding: 0px 8px;
+		}
 	}
 </style>
--- src/lib/Nav/NavButton.svelte
+++ src/lib/Nav/NavButton.svelte
@@ -1,36 +1,41 @@
 <!-- NavButton: Allows users to navigate to a new page -->
 <script lang="ts">
-    export let buttonText: string
-    export let highlighted: boolean = false
+	export let buttonText: string;
+	export let highlighted: boolean = false;
 
-    // Update button colour based on dynamic store value
-    import { primaryColour } from '../../stores/colourStore'
+	// Update button colour based on dynamic store value
+	import { primaryColour } from "../../stores/colourStore";
 </script>
 
-<button 
-    style={`
+<button
+	style={`
     color:
-        ${highlighted 
-            ? $primaryColour 
-            : 'var(--neutral-500)'}; 
+        ${highlighted ? $primaryColour : "var(--neutral-500)"}; 
     --project-colour: ${$primaryColour}
     `}
 >
-    {buttonText}
+	{buttonText}
 </button>
 
-
 <style>
-    button {
-        background: none;
-        font-size: 1.25rem;
-        border: none;
-        padding-left: 8px;
-        padding-right: 8px;
-        padding-top: 4px;
-        padding-bottom: 4px;
-    }
-    button:focus {
-        outline: 2px solid var(--project-colour);
-    }
+	button {
+		background: none;
+		font-size: 1.25rem;
+		font-weight: 900;
+		border: none;
+		padding-left: 8px;
+		padding-right: 8px;
+		padding-top: 4px;
+		padding-bottom: 4px;
+		/* -webkit-text-stroke: 0.01px rgba(0, 0, 0, 0.1);
+		text-shadow:
+			1px 1px 0 #000,
+			-1px -1px 0 #000,
+			1px -1px 0 #000,
+			-1px 1px 0 #000,
+			1px 1px 0 #000; */
+	}
+	button:focus {
+		outline: 2px solid var(--project-colour);
+	}
 </style>
